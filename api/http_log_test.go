1dec248edd0d7b27d6b5bfcce19ed8762335428e
effi <effoeffi@gmail.com>
2021-04-13 16:45:46 +0300

    TestOriginHasResponse added

--- api/http_log_test.go
+++ api/http_log_test.go
@@ -10,6 +10,60 @@ import (
 	"github.com/tufin/sabik/api"
 )
 
+func TestOriginHasResponse(t *testing.T) {
+
+	require.False(t, api.OriginHasResponse(api.OriginNginx))
+}
+
+func TestClone_RequestHeaders(t *testing.T) {
+
+	log := getLog(t)
+	logCopy := log
+	clone := log.Clone()
+
+	delete(log.RequestHeaders, "A")
+
+	require.NotEqual(t, getLog(t).RequestHeaders, logCopy.RequestHeaders)
+	require.Equal(t, getLog(t), clone)
+}
+
+func TestClone_ResponseHeaders(t *testing.T) {
+
+	log := getLog(t)
+	logCopy := log
+	clone := log.Clone()
+
+	log.ResponseHeaders.Add("x", "y")
+
+	require.NotEqual(t, getLog(t), logCopy)
+	require.Equal(t, getLog(t), clone)
+}
+
+func TestClone_QueryString(t *testing.T) {
+
+	log := getLog(t)
+	logCopy := log
+	clone := log.Clone()
+
+	log.QueryString.Del("a")
+
+	require.NotEqual(t, getLog(t), logCopy)
+	require.Equal(t, getLog(t), clone)
+}
+
+func TestClone_Cookies(t *testing.T) {
+
+	log := getLog(t)
+	logCopy := log
+	clone := log.Clone()
+
+	log.Cookies[0].Unparsed[0] = "x"
+
+	newLog := getLog(t)
+	require.NotEqual(t, newLog.Cookies[0].Unparsed, logCopy.Cookies[0].Unparsed)
+	require.Equal(t, newLog.Cookies[0].Unparsed, clone.Cookies[0].Unparsed)
+}
+
 func getLog(t *testing.T) *api.HTTPLog {
 	time, err := civil.ParseDateTime("2006-01-02t15:04:05.999999999")
 	require.NoError(t, err)
@@ -51,53 +105,4 @@ func getLog(t *testing.T) *api.HTTPLog {
 		Connection:      "a",
 		Origin:          "a",
 	}
-}
-
-func TestClone_RequestHeaders(t *testing.T) {
-
-	log := getLog(t)
-	copy := log
-	clone := log.Clone()
-
-	delete(log.RequestHeaders, "A")
-
-	require.NotEqual(t, getLog(t).RequestHeaders, copy.RequestHeaders)
-	require.Equal(t, getLog(t), clone)
-}
-
-func TestClone_ResponseHeaders(t *testing.T) {
-
-	log := getLog(t)
-	copy := log
-	clone := log.Clone()
-
-	log.ResponseHeaders.Add("x", "y")
-
-	require.NotEqual(t, getLog(t), copy)
-	require.Equal(t, getLog(t), clone)
-}
-
-func TestClone_QueryString(t *testing.T) {
-
-	log := getLog(t)
-	copy := log
-	clone := log.Clone()
-
-	log.QueryString.Del("a")
-
-	require.NotEqual(t, getLog(t), copy)
-	require.Equal(t, getLog(t), clone)
-}
-
-func TestClone_Cookies(t *testing.T) {
-
-	log := getLog(t)
-	copy := log
-	clone := log.Clone()
-
-	log.Cookies[0].Unparsed[0] = "x"
-
-	newLog := getLog(t)
-	require.NotEqual(t, newLog.Cookies[0].Unparsed, copy.Cookies[0].Unparsed)
-	require.Equal(t, newLog.Cookies[0].Unparsed, clone.Cookies[0].Unparsed)
 }
